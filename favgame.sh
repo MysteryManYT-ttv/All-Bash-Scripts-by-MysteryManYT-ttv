59080e4d0ed5fb74b3e02954a0de7a8c7d4e8e06
MysteryManYT-ttv <aidenrebec09@gmail.com>
2026-09-14 13:00:28 -0400

    Add files via upload

---
+++ favgame.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Favgame Copyright 2026 MysteryManYT-ttv
+
+BLUE="\033[0;34m"
+ENDCOLOR="\033[0m"
+
+read -p "what is your favorite game?: " FAVGAME1
+
+echo -e "${BLUE} $FAVGAME1 must be a fun game, Glad to know that $FAVGAME1 is a game you really enjoy! ${ENDCOLOR}" 
