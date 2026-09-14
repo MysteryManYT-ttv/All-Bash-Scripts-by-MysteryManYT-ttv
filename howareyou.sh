f42c8271836c8087115f8f5a56e5baebd8230ddc
MysteryManYT-ttv <aidenrebec09@gmail.com>
2026-09-14 12:56:27 -0400

    Add files via upload

---
+++ howareyou.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Copyright MysteryManYT-ttv
+
+BLUE="\033[0;34m"
+ENDCOLOR="\033[0m"
+
+read -p "how are you?: " HOWAREYOU1
+
+echo -e "${BLUE}Nice to know you are feeling $HOWAREYOU1 , hope you have a good rest of your day! :)${ENDCOLOR}"
+
