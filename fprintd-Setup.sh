4dd0a79fbffa6e6c41f4f36f3dadc433c45e380a
MysteryManYT-ttv <aidenrebec09@gmail.com>
2026-09-14 10:41:52 -0400

    Add files via upload

---
+++ fprintd-Setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+
+#Copyright MysteryManYT-ttv 2025
+#I write these to help me and maybe others
+#This will only work if you have a Fingerprint scanner.
+
+sudo apt upgrade && sudo apt install -y fprintd libpam-fprintd
+sudo pam-auth-update && sudo fwupdmgr refresh && sudo fwupdmgr update
+echo "Nothing working? Check BIOS to make sure the fingerprint sensor is enabled."
+fprint-enroll
+#You will see something like this
+#PAM profiles to enable:                                          
+ #│                                                               
+ #│  [ ] Fingerprint authentication <--                                         
+ #│  [*] Unix authentication                                         
+ #│  [*] Register user sessions in the systemd control group ... 
+
+#Press Space to enable/disable, arrow keys to navigate, Tab to Select <Ok> or <Cancel>. then press enter on selected option.
+
+
+
+
+
+
+#What to do If sudo pam-auth-update fails
+#Ensure you run sudo apt install libpam-fprintd specifically.
+#If you get any error that says something like device not found, run the following
+#sudo fwupdmgr refresh
+#sudo fwupdmgr update
+#fprint-enroll
+#then reboot
+
+
+#Is libpam-fprintd installed? (Required for the pam-auth-update checkbox to appear).
+ 
+
